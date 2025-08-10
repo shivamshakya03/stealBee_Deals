583e17a5f72d34b89a6f6b3637e862f0a29a82f7
Shivam Shakya <shivamshakya33871@gmail.com>
2025-08-10 19:59:57 +0530

    update product list UI and index.html

--- client/src/product/ProductList/ProductList.jsx
+++ client/src/product/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import DealsSection from "../../ui/DealSections/DealsSection.jsx";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../../redux/features/products/productSlice";
